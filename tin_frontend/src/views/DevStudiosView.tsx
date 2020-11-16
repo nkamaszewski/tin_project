9f049f98e93789886f2fb6409d67baa31aac852e
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2020-11-16 09:05:01 +0100

    wip edit mode

--- tin_frontend/src/views/DevStudiosView.tsx
+++ tin_frontend/src/views/DevStudiosView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import styled from "styled-components";
 import Card from "../components/Card";
@@ -22,32 +22,51 @@ const DevStudiosViewStyled = styled.div`
     transform: scale(1.02);
     color: #ffd000;
   }
+
+  .addBtn {
+    width: 120px;
+    height: 36px;
+    cursor: pointer;
+  }
 `;
 
 const DevStudiosView = () => {
+  const [isEditMode, setIsEditMode] = useState(false);
   const history = useHistory();
+  const handleOnButtonClick = () =>
+    setIsEditMode((prevState: boolean) => !prevState);
   const handleOnCardContainerClick = (id: string) =>
     history.push(`/devstudios/${id}`);
   return (
     <DevStudiosViewStyled>
-      <header className="listHeader">
-        <p>name:</p>
-      </header>
-      {mockDevStudios.map(({ id, name }: DevStudio) => (
-        <div
-          key={id}
-          className="cardContainer"
-          onClick={() => {
-            handleOnCardContainerClick(id);
-          }}
-        >
-          <Card style={{ marginBottom: "12px" }}>
-            <section className="gameContent">
-              <p>{name}</p>
-            </section>
-          </Card>
-        </div>
-      ))}
+      <button className="addBtn" onClick={handleOnButtonClick}>
+        Add +
+      </button>
+      {isEditMode ? (
+        <div>edit mode</div>
+      ) : (
+        <>
+          {" "}
+          <header className="listHeader">
+            <p>name:</p>
+          </header>
+          {mockDevStudios.map(({ id, name }: DevStudio) => (
+            <div
+              key={id}
+              className="cardContainer"
+              onClick={() => {
+                handleOnCardContainerClick(id);
+              }}
+            >
+              <Card style={{ marginBottom: "12px" }}>
+                <section className="gameContent">
+                  <p>{name}</p>
+                </section>
+              </Card>
+            </div>
+          ))}
+        </>
+      )}
     </DevStudiosViewStyled>
   );
 };
--- tin_frontend/src/views/GamesView.tsx
+++ tin_frontend/src/views/GamesView.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import GamesList from "../components/Lists/GamesList";
 import { mockGames } from "../mocks/mockGames";
 
 const GamesViewStyled = styled.div`
   padding: 32px;
+  .addBtn {
+    width: 120px;
+    height: 36px;
+    cursor: pointer;
+  }
 `;
 
 const GamesView = () => {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const handleOnButtonClick = () =>
+    setIsEditMode((prevState: boolean) => !prevState);
   return (
     <GamesViewStyled>
-      <GamesList list={mockGames} />
+      <button className="addBtn" onClick={handleOnButtonClick}>
+        Add +
+      </button>
+      {isEditMode ? <div>edit mode </div> : <GamesList list={mockGames} />}
     </GamesViewStyled>
   );
 };
